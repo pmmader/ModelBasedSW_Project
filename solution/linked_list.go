e024d0e88ff3046ef6b57888dcac33e862986655
Hagen <h.murki@t-online.de>
2024-06-23 12:01:29 +0200

    Added monomophization for bool and float for the respective functions

--- solution/linked_list.go
+++ solution/linked_list.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-//Example linked list
+// Example linked list
 type node[T any] struct {
 	val  T
 	next *node[T]
@@ -53,28 +53,28 @@ func testNode() {
 
 }
 
-//Example linked list translated by Monomorphization
-type node_mono struct {
+// Example linked list translated by Monomorphization
+type node_mono_int struct {
 	val  int
 	next *node_mono
 }
 
-func mkNode_mono(v int) *node_mono {
-	return &node_mono{val: v, next: nil}
+func mkNode_mono_int(v int) *node_mono {
+	return &node_mono_int{val: v, next: nil}
 }
 
-func insert_mono(n *node_mono, v int) {
-	n.next = mkNode_mono(v)
+func insert_mono_int(n *node_mono_int, v int) {
+	n.next = mkNode_mono_int(v)
 }
 
 // n > 0
-func mkList_mono(n int, v int) *node_mono {
+func mkList_mono_int(n int, v int) *node_mono_int {
 
-	head := mkNode_mono(v)
+	head := mkNode_mono_int(v)
 	current := head
 
 	for i := 0; i < n-1; i++ {
-		insert_mono(current, v)
+		insert_mono_int(current, v)
 		current = current.next
 
 	}
@@ -82,7 +82,7 @@ func mkList_mono(n int, v int) *node_mono {
 
 }
 
-func len_mono(n *node_mono) int {
+func len_mono_int(n *node_mono_int) int {
 	i := 0
 
 	for n != nil {
@@ -95,11 +95,61 @@ func len_mono(n *node_mono) int {
 
 }
 
-func testNode_mono() {
-	n1 := mkList_mono(10, 1)
-	fmt.Printf("\n%d", len_mono(n1))
+func testNode_mono_int() {
+	n1 := mkList_mono_int(10, 1)
+	fmt.Printf("\n%d", len_mono_int(n1))
 
 }
+
+
+
+type node_mono_bool struct {
+	val  bool
+	next *node_mono
+}
+
+func mkNode_mono_bool(v int) *node_mono_bool {
+	return &node_mono_boolval: v, next: nil}
+}
+
+func insert_mono_bool(n *node_mono_bool, v bool) {
+	n.next = mkNode_mono_bool(v)
+}
+
+// n > 0
+func mkList_mono_bool(n int, v bool) *node_mono_bool {
+
+	head := mkNode_mono_bool(v)
+	current := head
+
+	for i := 0; i < n-1; i++ {
+		insert_mono_bool(current, v)
+		current = current.next
+
+	}
+	return head
+
+}
+
+func len_mono_bool(n *node_mono_bool) int {
+	i := 0
+
+	for n != nil {
+		i++
+		n = n.next
+
+	}
+
+	return i
+
+}
+
+func testNode_mono_bool() {
+	n1 := mkList_mono_bool(10, 1)
+	fmt.Printf("\n%d", len_mono_bool(n1))
+
+
+
 
 // with Example linked list Generic translation
 type node_gen struct {
@@ -152,7 +202,7 @@ func testNode_gen() {
 
 }
 
-//Example summing up numeric values
+// Example summing up numeric values
 func sum[T int | float32](xs []T) T {
 	var x T
 	x = 0
@@ -165,8 +215,8 @@ func sum[T int | float32](xs []T) T {
 
 }
 
-//Example summing up numeric values Monorization
-func sum_mono(xs []int) int {
+// Example summing up numeric values Monorization
+func sum_mono_int(xs []int) int {
 	var x int
 	x = 0
 	for _, v := range xs {
@@ -177,6 +227,20 @@ func sum_mono(xs []int) int {
 	return x
 
 }
+
+
+
+func sum_mono_float32(xs []float32) float32 {
+	var x float32
+	x = 0
+	for _, v := range xs {
+
+		x = x + v
+	}
+
+	return x
+}
+
 
 /*/Example summing up numeric values Generics
 func sum_gen(xs []interface{}) interface{} {
@@ -191,21 +255,21 @@ func sum_gen(xs []interface{}) interface{} {
 
 }*/
 
-//Example swap
+// Example swap
 func swap[T any](x *T, y *T) {
 	tmp := *x
 	*x = *y
 	*y = tmp
 }
 
-//Example swap Monorization
+// Example swap Monorization
 func swap_mono(x *int, y *int) {
 	tmp := *x
 	*x = *y
 	*y = tmp
 }
 
-//Example swap Generics
+// Example swap Generics
 func swap_gen(x *interface{}, y *interface{}) {
 	tmp := *x
 	*x = *y
